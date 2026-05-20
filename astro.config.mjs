6fc23c8a921e70691ebb2916c97d570e73a2d2cb
yamanidev <elamine.yamani@gmail.com>
2026-05-20 20:52:45 +0100

    fix: inline CSS to avoid FOUC

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://html-reducer.yamani.dev",
+  build: {
+    inlineStylesheets: "always",
+  },
 });
